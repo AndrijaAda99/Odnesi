8a641e90d35e353777d440d2de7dd5880451d35b
idzii <mi18185@alas.matf.bg.ac.rs>
2022-04-14 18:39:13 +0200

    Add: folium map

--- odnesi_app/view.py
+++ odnesi_app/view.py
@@ -1,9 +1,11 @@
+from tracemalloc import start
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 
 from odnesi_app.db import get_db
 
+import folium
 
 bp = Blueprint('view', __name__)
 
@@ -16,5 +18,7 @@ def index():
     wastes = db.execute(
         'SELECT name, location, culture, amount, phone_number FROM waste;'
     ).fetchall()
-    return render_template('view.html', companies=companies, wastes=wastes)
 
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return render_template('view.html', companies=companies, wastes=wastes, map=folium_map)
